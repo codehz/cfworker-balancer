cbf655164eacfcdabeff3fc53148ea69a9c49004
codehz <git@hertz.moe>
2024-03-10 12:05:46 +0800

    update status page

--- src/status.ts
+++ src/status.ts
@@ -1,26 +1,23 @@
-import { classnames, html, on } from "@codehz/mutable-element";
+import { classnames, html, mutate, on } from "@codehz/mutable-element";
 import { create } from "css-in-bun" with { type: "macro" };
 
 export function* Status() {
-  const iframe = html`iframe[src=/statusz]`(
-    classnames(styles.iframe),
-    on("load", () => {
-      const content = iframe.contentDocument!;
-      iframe.style.height = content.documentElement.scrollHeight + "px";
-    })
-  ) as HTMLIFrameElement;
+  const output = html`code`(load);
   yield html`button`(
     "刷新",
-    on("click", () => iframe.contentWindow?.location.reload())
+    on("click", () => mutate(output, load))
   );
-  yield iframe;
+  yield html`pre`(classnames(styles.Output), output);
 }
 
 const styles = create({
-  iframe: {
-    width: "100%",
-    height: 600,
+  Output: {
     marginTop: 10,
-    transition: "height 0.1s ease",
   },
 });
+
+async function load(this: HTMLElement) {
+  console.log("here");
+  const res = await fetch("/statusz");
+  return await res.text();
+}
