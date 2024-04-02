9af2185664bfbb83819b6cd0dc8a5b0b35d385c2
codehz <git@hertz.moe>
2024-04-02 16:19:06 +0800

    add cf ip to trusted

--- srv/.init.lua
+++ srv/.init.lua
@@ -3,6 +3,21 @@ local alist = require 'alist'
 local auth = require 'auth'
 
 ProgramBrand('Cloudflare Worker Balancer')
+ProgramTrustedIp(ParseIp("103.21.244.0"), 22)
+ProgramTrustedIp(ParseIp("103.22.200.0"), 22)
+ProgramTrustedIp(ParseIp("103.31.4.0"), 22)
+ProgramTrustedIp(ParseIp("104.16.0.0"), 13)
+ProgramTrustedIp(ParseIp("104.24.0.0"), 14)
+ProgramTrustedIp(ParseIp("108.162.192.0"), 18)
+ProgramTrustedIp(ParseIp("131.0.72.0"), 22)
+ProgramTrustedIp(ParseIp("141.101.64.0"), 18)
+ProgramTrustedIp(ParseIp("162.158.0.0"), 15)
+ProgramTrustedIp(ParseIp("172.64.0.0"), 13)
+ProgramTrustedIp(ParseIp("173.245.48.0"), 20)
+ProgramTrustedIp(ParseIp("188.114.96.0"), 20)
+ProgramTrustedIp(ParseIp("190.93.240.0"), 20)
+ProgramTrustedIp(ParseIp("197.234.240.0"), 22)
+ProgramTrustedIp(ParseIp("198.41.128.0"), 17)
 
 function OnServerHeartbeat()
   reload()
